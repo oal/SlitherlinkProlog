d7609a0c5821d88f7156fe943d1e8238dc83b7d2
Olav Lindekleiv <olav@lindekleiv.com>
2016-11-21 19:06:25 +0100

    Tried replacing all four element lists with cell structure, but text board takes 10-15% longer to solve. Probably not worth continuing.

--- SlitherlinkRead.pl
+++ SlitherlinkRead.pl
@@ -26,23 +26,23 @@ cell(3, 0, 1, 1, 1).
 cell(4, 1, 1, 1, 1).
 cell(?, T, R, B, L):- cell(1, T, R, B, L); cell(2, T, R, B, L); cell(0, T, R, B, L); cell(3, T, R, B, L); cell(4, T, R, B, L).
 
-genCell(N, [T, R, B, L]):-
+genCell(N, cell(_, T, R, B, L)):-
     cell(N, T, R, B, L).
 
-getCell(X, Y, Input, Board, TRBL):-
-    nth0(Y, Input, RowI), nth0(X, RowI, N), genCell(N, TRBL).
+getCell(X, Y, Input, Board, cell(N, T, R, B, L)):-
+    nth0(Y, Input, RowI), nth0(X, RowI, N), cell(N, T, R, B, L).
 
 getSquare(X, Y, SizeX, SizeY, Input, Board, Square):-
-    getCell(X, Y, Input, Board, [AT, AR, AB, AL]),
+    getCell(X, Y, Input, Board, cell(_, AT, AR, AB, AL)),
 
     X1 is X+1,
-    getCell(X1, Y, Input, Board, [BT, BR, BB, BL]),
+    getCell(X1, Y, Input, Board, cell(_, BT, BR, BB, BL)),
     AR=BL, 
     TopEdge is AT+BT+AR,
     TopEdge \= 3,
 
     Y1 is Y+1,
-    getCell(X, Y1, Input, Board, [CT, CR, CB, CL]),
+    getCell(X, Y1, Input, Board, cell(_, CT, CR, CB, CL)),
     AB=CT,
     LeftEdge is AL+CL+AB,
     LeftEdge \= 3,
@@ -50,7 +50,7 @@ getSquare(X, Y, SizeX, SizeY, Input, Board, Square):-
     Crossing is (AB+AR+CR+BB), % center cross
     crossing(Crossing),
 
-    getCell(X1, Y1, Input, Board, [DT, DR, DB, DL]),
+    getCell(X1, Y1, Input, Board, cell(_, DT, DR, DB, DL)),
     BB=DT, CR=DL,
 
     RightEdge is BR+BB+DR,
@@ -59,54 +59,54 @@ getSquare(X, Y, SizeX, SizeY, Input, Board, Square):-
     BottomEdge \= 3,
 
     Square = [
-        [[AT, AR, AB, AL], [BT, BR, BB, BL]],
-        [[CT, CR, CB, CL], [DT, DR, DB, DL]]
+        [cell(_, AT, AR, AB, AL), cell(_, BT, BR, BB, BL)],
+        [cell(_, CT, CR, CB, CL), cell(_, DT, DR, DB, DL)]
     ].
 
-validateTop([A, B, _, _]):-
-    A = [AT, AR, _, _],
-    B = [BT, _, _, _],
+validateTop(cell(_, A, B, _, _)):-
+    A = cell(_, AT, AR, _, _),
+    B = cell(_, BT, _, _, _),
     Crossing is (AT+AR+BT),
     crossing(Crossing).
-validateRight([_, B, _, D]):-
-    B = [_, BR, BB, _],
-    D = [_, DR, _, _],
+validateRight(cell(_, _, B, _, D)):-
+    B = cell(_, _, BR, BB, _),
+    D = cell(_, _, DR, _, _),
     Crossing is (BR+BB+DR),
     crossing(Crossing).
-validateBottom([_, _, C, D]):-
-    C = [_, CR, CB, _],
-    D = [_, _, DB, _],
+validateBottom(cell(_, _, _, C, D)):-
+    C = cell(_, _, CR, CB, _),
+    D = cell(_, _, _, DB, _),
     Crossing is (CB+CR+DB),
     crossing(Crossing).
-validateLeft([A, _, C, _]):-
-    A = [_, _, AB, AL],
-    C = [_, _, _, CL],
+validateLeft(cell(_, A, _, C, _)):-
+    A = cell(_, _, _, AB, AL),
+    C = cell(_, _, _, _, CL),
     Crossing is (AL+AB+CL),
     crossing(Crossing).
 
 
-validateTopLeft([A, B, C, D]):-
-    validateTop([A, B, C, D]),
-    validateLeft([A, B, C, D]),
-    A = [AT, _, _, AL],
+validateTopLeft(cell(_, A, B, C, D)):-
+    validateTop(cell(_, A, B, C, D)),
+    validateLeft(cell(_, A, B, C, D)),
+    A = cell(_, AT, _, _, AL),
     Crossing is (AT+AL),
     crossing(Crossing).
-validateTopRight([A, B, C, D]):-
-    validateTop([A, B, C, D]),
-    validateRight([A, B, C, D]),
-    B = [BT, BR, _, _],
+validateTopRight(cell(_, A, B, C, D)):-
+    validateTop(cell(_, A, B, C, D)),
+    validateRight(cell(_, A, B, C, D)),
+    B = cell(_, BT, BR, _, _),
     Crossing is (BT+BR),
     crossing(Crossing).
-validateBottomRight([A, B, C, D]):-
-    validateBottom([A, B, C, D]),
-    validateRight([A, B, C, D]),
-    D = [_, DR, DB, _],
+validateBottomRight(cell(_, A, B, C, D)):-
+    validateBottom(cell(_, A, B, C, D)),
+    validateRight(cell(_, A, B, C, D)),
+    D = cell(_, _, DR, DB, _),
     Crossing is (DR+DB),
     crossing(Crossing).
-validateBottomLeft([A, B, C, D]):-
-    validateBottom([A, B, C, D]),
-    validateLeft([A, B, C, D]),
-    C = [_, _, CB, CL],
+validateBottomLeft(cell(_, A, B, C, D)):-
+    validateBottom(cell(_, A, B, C, D)),
+    validateLeft(cell(_, A, B, C, D)),
+    C = cell(_, _, _, CB, CL),
     Crossing is (CB+CL),
     crossing(Crossing).
 
@@ -121,14 +121,14 @@ validateSquare(X, Y, SizeX, SizeY, Input, Board):-
     SizeY1 is SizeY-2,
     (
         X > 0,       Y > 0,       X < SizeX1, Y < SizeY1;
-        X = 0,       Y = 0,       validateTopLeft([A, B, C, D]);
-        X = SizeX1,  Y = 0,       validateTopRight([A, B, C, D]);
-        X = SizeX1,  Y = SizeY1,  validateBottomRight([A, B, C, D]);
-        X = 0,       Y = SizeY1,  validateBottomLeft([A, B, C, D]);
-        X \= 0,      X \= SizeX1, Y = 0,       validateTop([A, B, C, D]);
-        X = SizeX1,  Y \= 0,      Y \= SizeY1, validateRight([A, B, C, D]);
-        X \= 0,      X \= SizeX1, Y = SizeY1,  validateBottom([A, B, C, D]);
-        X = 0,       Y \= 0,      Y \= SizeY1,  validateLeft([A, B, C, D])
+        X = 0,       Y = 0,       validateTopLeft(cell(_, A, B, C, D));
+        X = SizeX1,  Y = 0,       validateTopRight(cell(_, A, B, C, D));
+        X = SizeX1,  Y = SizeY1,  validateBottomRight(cell(_, A, B, C, D));
+        X = 0,       Y = SizeY1,  validateBottomLeft(cell(_, A, B, C, D));
+        X \= 0,      X \= SizeX1, Y = 0,       validateTop(cell(_, A, B, C, D));
+        X = SizeX1,  Y \= 0,      Y \= SizeY1, validateRight(cell(_, A, B, C, D));
+        X \= 0,      X \= SizeX1, Y = SizeY1,  validateBottom(cell(_, A, B, C, D));
+        X = 0,       Y \= 0,      Y \= SizeY1,  validateLeft(cell(_, A, B, C, D))
     ),
 
     length(RowsBefore, Y),
@@ -162,7 +162,7 @@ simplifyBoard([Row|Rest], NewSimpleBoard):-
     maplist(getLeft, Row, Lefts),
 
     % last | if set.
-    last(Row, [_, R, _, _]),
+    last(Row, cell(_, _, R, _, _)),
 
     append(Lefts, [R], Verticals),
 
@@ -251,10 +251,10 @@ vChar(0, ' ').
 vChar(1, '|').
 vChar(_, '?').
 
-getTop([T, _, _, _], T).
-getRight([_, R, _, _], R).
-getBottom([_, _, B, _], B).
-getLeft([_, _, _, L], L).
+getTop(cell(_, T, _, _, _), T).
+getRight(cell(_, _, R, _, _), R).
+getBottom(cell(_, _, _, B, _), B).
+getLeft(cell(_, _, _, _, L), L).
 
 writeHorizontal([]):-
     write('+'), nl.
@@ -313,11 +313,11 @@ continueInt(N,N).
 is_number_code(N, N1):- N>=48, N<58, N1 is N-48.
 
 /*********************** global control: starting the algorithm and the reading */
-run:- inputFile(IF), see(IF), outputFile(F), tell(F), readInt(N), write(N), nl, solveProblems(N), told, seen, !.
+run:- inputFile(IF), see(IF), outputFile(F), /*tell(F),*/ readInt(N), write(N), nl, solveProblems(N), told, seen, !.
 run:- told, seen. /* close the files */
 
 solveProblems(0).
-solveProblems(N):- N>0, readProblem(X, Y, I), doSolve(X, Y, I, S), writeFullOutput(S, X, Y), !, N1 is N-1, solveProblems(N1).
+solveProblems(N):- N>0, readProblem(X, Y, I), time(doSolve(X, Y, I, S)), writeFullOutput(S, X, Y), !, N1 is N-1, solveProblems(N1).
 
 :- nl,nl,write(' try running "?- run."'), nl,nl,nl.
 
